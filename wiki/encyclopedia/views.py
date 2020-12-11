f59f3b0c239a979cfd286f59a3e1706d6075aac9
Nicolas Honda Takeda <ntakeda@Nicolass-MacBook-Air.local>
2020-12-11 09:43:54 -0300

    entry page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from markdown2 import markdown
 
 from . import util
 
@@ -8,3 +10,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki(request, name):
+    if not util.get_entry(name):
+        return render(request, "encyclopedia/entry.html", {
+            "name": name,
+            "content": "Not found"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "name": name,
+            "content": markdown(util.get_entry(name))
+        })
+        #return HttpResponse(markdown(util.get_entry(name)))
