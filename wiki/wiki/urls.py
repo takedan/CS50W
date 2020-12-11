ce3cdb29b50a8bde4412c21d7dfeb5218a16268f
Nicolas Honda Takeda <ntakeda@Nicolass-MacBook-Air.local>
2020-12-11 08:42:10 -0300

    inicio

--- wiki/wiki/urls.py
+++ wiki/wiki/urls.py
@@ -1,7 +1,7 @@
-"""lecture3 URL Configuration
+"""wiki URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,7 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls')),
-    path('newyear/', include('newyear.urls')),
-    path('tasks/', include('tasks.urls'))
+    path('', include("encyclopedia.urls"))
 ]
