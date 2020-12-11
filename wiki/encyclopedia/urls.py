2376b6a483d07112a74f3406085fef42773dae62
Nicolas Honda Takeda <ntakeda@Nicolass-MacBook-Air.local>
2020-12-11 11:38:24 -0300

    search page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.wiki, name="wiki")
+    path("<str:name>", views.wiki, name="wiki"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from markdown2 import markdown
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        if util.get_entry(name):
+            return wiki(request, name)
+        entry_list = util.list_entries()
+        list = []
+        for entry in entry_list:
+            if entry.upper().find(name.upper()) >=0:
+                list.append(entry)
+        if len(list)>0:
+            return render(request, "encyclopedia/index.html", {
+                "entries": list
+            })
+        else:
+            return render(request, "encyclopedia/entry.html", {
+                "name": name,
+                "content": "Not found"
+            })
+    else:   
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
 
 def wiki(request, name):
     if not util.get_entry(name):
@@ -21,4 +40,3 @@ def wiki(request, name):
             "name": name,
             "content": markdown(util.get_entry(name))
         })
-        #return HttpResponse(markdown(util.get_entry(name)))
