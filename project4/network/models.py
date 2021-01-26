2a46c6ac73257eed4d867f6922ca4150e8a63263
Nicolas Honda Takeda <ntakeda@gmail.com>
2021-01-26 15:49:15 -0300

    modelos

---
+++ project4/network/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="usuarios")
+    text = models.TextField(max_length=280)
+    likes = models.IntegerField()
+    datecreation = models.DateField(auto_now=True)    
+
+    def __str__(self):
+        return f"{self.user} POST N {self.id}"
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="usuarios")
+    list = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f"{seld.user}"
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="usuarios")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="posts")
+    text = models.TextField(max_lenght=280)
+    likes = models.IntegerField()
+    datecreation = models.Datefield(auto_now=True)
